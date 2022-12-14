026a250324cb79c0a38ee54ba6e54b27ddf76838
davidmunene <davidmunene.dm@gmail.com>
2022-12-14 21:23:40 +0000

    Client configuration file - Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,10 @@
-# config ssh_config
-file_line {'pass auten No':
-  ensure => present,
-    path => '/etc/ssh/ssh_config',
-    line => ' PasswordAuthentication no',
-    match=> 'PasswordAuthentication yes',
+# Set the config of a ssh_file
+file_line { 'Change the main private key':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
 }
-file_line {'Add ident file':
-  ensure => present,
-    path => '/etc/ssh/ssh_config',
-    line => ' IdentityFile ~/.ssh/holberton',
+
+file_line { 'No Authenticate with passowrd':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
 }
