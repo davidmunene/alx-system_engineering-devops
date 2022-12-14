d1adba6deae3d78a0c64c26c7b291edd51b07623
davidmunene <davidmunene.dm@gmail.com>
2022-12-14 06:54:30 +0000

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# This is a puppet manifest that creates a file in /tmp directory
+
+
+file { 'school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content =>  'I love Puppet'
+}
