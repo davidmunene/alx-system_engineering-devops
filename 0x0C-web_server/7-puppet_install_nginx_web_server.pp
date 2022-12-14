ebcb98575e425825bfd29d82a210a6ac767bf5f0
davidmunene <davidmunene.dm@gmail.com>
2022-12-14 21:55:53 +0000

    Nginx Web Server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,23 @@
-# Install and configure nginx
-package { 'jfryman-nginx':
-  ensure => installed,
+# Install nginx with puppet
+package { 'nginx':
+  ensure   => '1.18.0',
+  provider => 'apt',
 }
 
-include nginx
-
-class { 'nginx':
-  manage_repo    => true,
-  package_source => 'nginx-stable',
-}
-
-nginx::resource::server { '100.25.182.248':
-  listen_port      => 80,
-  www_root         => '/var/www/html/',
-  vhost_cfg_append => { 'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent' },
-}
-
-file { 'index':
+file { 'Hello World':
   path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Hello World!',
+  content => 'Hello World',
+}
+
+file_line { 'Hello World':
+  path  => '/etc/nginx/sites-available/default',
+  after => 'server_name _;',
+  line  => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+exec { 'service':
+  command  => 'service nginx start',
+  provider => 'shell',
+  user     => 'root',
+  path     => '/usr/sbin/service',
 }
